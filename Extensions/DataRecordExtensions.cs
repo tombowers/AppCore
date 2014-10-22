ec8da3968ad023d541127a79f3a43573364d2581
Tom Bowers <tom.bowers@redrow.co.uk>
2014-10-22 17:28:14 +0100

    Added GetValueOrDefault extension for IDataRecord.

---
+++ Extensions/DataRecordExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+
+namespace RR.Core.Extensions
+{
+	public static class DataRecordExtensions
+	{
+		public static T GetValueOrDefault<T>(this IDataRecord dr, string name)
+		{
+			var value = dr[name];
+			if (DBNull.Value == value) return default(T);
+
+			if (typeof(T).IsEnum || typeof(T).IsNullableEnum())
+				return ((string)value).ToEnum<T>();
+
+			return (T)value;
+		}
+	}
+}
